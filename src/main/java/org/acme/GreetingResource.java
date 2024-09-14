085567a5b87e34019b62070c8eacb80f855ff1f1
jjsn10 <san14038>
2024-09-14 10:31:19 -0600

    1.4 Quarkus Endpoints Practice

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -13,4 +13,11 @@ public class GreetingResource {
     public String hello() {
         return "Hello RESTEasy";
     }
+
+    @Path("/personalized")
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String personalizedHello(String name) {
+        return "Hello " + name;
+    }
 }
