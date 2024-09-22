653224af519a075eabae882e1ecdf3cae5127e32
jjsn10 <san14038>
2024-09-22 11:53:15 -0600

    1.7 Connecting the Frontend to the Backend

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,24 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from "react";
+import './App.css'
 
-function App() {
+function App(){
+
+  const [message, setMessage] = useState("");
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  //const [submittedData, setSubmittedData] = useState(null);
+
+  const handleSubmit = async (e)=>{
+    e.preventDefault();
+    //const data = {firstName, lastName};
+    //console.log(data)
+    try{
+      const response = await fetch(`/hello/personalized`, {
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({first: firstName, last: lastName}),
+      });
+
+      if(response.ok){
+        //const result = await response.json();
+        const result = await response.text()
+        setMessage(result)
+        setFirstName('')
+        setLastName('')
+        //console.log(result);
+
+      }else{
+        console.error('Error submitting form:',response.statusText);
+      }
+    }catch(error){
+      console.error('Error:',error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="container">
+        <div className="form-wrapper">
+        <form onSubmit={handleSubmit}>
+          <h2>Personalized Greeting</h2>
+          <div className="name">
+            <label>First Name: </label>
+            <input
+                type="text"
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
+            />
+          </div>
+          <div className="last">
+            <label>Last Name: </label>
+            <input
+                type="text"
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
+            />
+          </div>
+          <button type="submit" className="submit">Submit</button>
+        </form>
+        </div>
+        {message && (
+            <div>
+            <p>{message}</p>
+          </div>
+      )}
+      </div>
   );
 }
 
